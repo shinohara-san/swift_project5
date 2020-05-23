2da26a407555f926f5c4e595d4f41cac11446b36
shinohara-san <someiyoshino0809@gmail.com>
2020-05-23 13:51:17 +0900

    first commit

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -8,13 +8,147 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    var tooShort = false
+    var sameStart = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
     }
-
-
+    
+    @objc func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer: answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String, lengthCheck: Bool) -> Bool {
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count) //utf16 emojiとの兼ね合い
+        
+        if range.length < 3 {
+            tooShort = true
+            return false
+        }
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        tooShort = false
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func notSame(word: String, sameStart: Bool) -> Bool{
+        guard let tempWord = title?.lowercased() else { return false }
+        let startWord = tempWord.prefix(word.count)
+        if word == startWord{
+            self.sameStart = true
+            return false
+        }
+        self.sameStart = false
+        return true
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String){
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer, lengthCheck: !tooShort) {
+                    if notSame(word: lowerAnswer, sameStart: !sameStart){
+                        usedWords.insert(lowerAnswer, at: 0)
+                        
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        
+                        return
+                        
+                    } else {
+                        showErrorMessage(errorTitle: "Same prefix", errorMessage: "You cannot use the same prefix")
+                    }
+                } else {
+                    if tooShort{
+                        showErrorMessage(errorTitle: "Word too short", errorMessage: "Must be longer than three words")
+                    } else {
+                        showErrorMessage(errorTitle: "Word not recognised", errorMessage: "You can't just make them up, you know!")
+                    }
+                    
+                }
+            } else {
+                showErrorMessage(errorTitle: "Word used already", errorMessage: "Be more original!")
+                
+            }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            showErrorMessage(errorTitle: "Word not possible", errorMessage: "You can't spell that word from \(title)")
+        }
+        
+        
+    }
+    
 }
 
